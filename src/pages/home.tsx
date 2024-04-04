843fc62204df5c424a5806460442129078712a25
peter064226 <peter064226@hotmail.com>
2024-04-04 15:26:10 +0800

    feat: edit mode switch

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,17 +1,44 @@
-import { ProForm, ProFormText } from '@ant-design/pro-components';
-import logo from '../logo.svg';
+import { ProForm, ProFormRadio, ProFormText } from '@ant-design/pro-components';
+import { useState } from 'react';
 import './home.css';
+import { Button } from 'antd';
+import logo from '../logo.svg';
 
 function Home() {
+  const [readonly, setReadoly] = useState(false);
+
   return (
     <div className="profile-container">
-      <img src={logo} className="app-logo" alt="logo" />
-      <div className="app-link">User Profile</div>
-      <ProForm className="profile-form">
-        <ProFormText name="username" label="Username" />
-        <ProFormText name="email" label="Email" />
-        <ProFormText name="phone" label="Phone" />
+      <div className="profile-header">
+        <img src={logo} className="profile-logo" alt="logo" />
+        <div className="app-link">User Profile</div>
+      </div>
+      <ProForm
+        submitter={readonly ? false : { resetButtonProps: false }}
+        className="profile-form"
+        onFinish={async () => {
+          await setReadoly(true);
+        }}>
+        <ProFormText name="username" label="username" rules={[{ required: true }]} readonly={readonly} />
+        <ProFormText name="email" label="email" rules={[{ required: true }, { type: 'email' }]} readonly={readonly} />
+        <ProFormText name="phone" label="phone" rules={[{ required: true }]} readonly={readonly} />
+        <ProFormRadio.Group
+          label="gender"
+          valueEnum={{
+            1: 'Male',
+            0: 'Female',
+          }}
+          name="gender"
+          rules={[{ required: true }]}
+          readonly={readonly}
+        />
+        <ProFormText name="address" label="address" rules={[{ required: true }]} readonly={readonly} />
       </ProForm>
+      {readonly && (
+        <Button type="primary" onClick={() => setReadoly(false)}>
+          Edit
+        </Button>
+      )}
     </div>
   );
 }
